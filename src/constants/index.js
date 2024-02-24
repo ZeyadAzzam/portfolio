00369b7e3a4faff64e370ccfe12e610f7be0f5b4
ziadazam2001 <zeyad.a@wtm.llc>
2024-02-24 19:02:35 +0300

    project init

---
+++ src/constants/index.js
@@ -0,0 +1,259 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  nestjs,
+  springboot,
+  blulogix,
+  wtm
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "Nestjs",
+    icon: nestjs,
+  },
+  {
+  name: "Spring Boot",
+  icon: springboot
+  }
+];
+
+const experiences = [
+  {
+    title: "Full Stack Developer",
+    company_name: "InterShip: BluLogix",
+    icon: blulogix,
+    iconBg: "#383E56",
+    date: "June 2023 - Oct 2023",
+    points: [
+      "Developing and maintaining web applications using Angular and Springboot and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility."
+        ],
+  },
+  {
+    title: "Full Stack Developer",
+    company_name: "InternShip: We.The.Makers",
+    icon: wtm,
+    iconBg: "#332c6b",
+    date: "Nov 2023 - Feb 2023",
+    points: [
+      "Developing and maintaining web applications using React.js,NestJs,React Native Also frameworks for styles such as: tailwind css and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    "title": "Back-End Developer",
+    "company_name": "We.The.Makers",
+    "icon": wtm,
+    "iconBg": "#332c6b",
+    "date": "Feb 2024 - Current",
+    "points": [
+        "Designing, developing, and maintaining robust back-end solutions for web applications using Nestjs, PostgreSQL, Prisma, and RefineJS.",
+        "Collaborating closely with cross-functional teams, including designers, product managers, and fellow developers, to ensure the successful delivery of high-quality products.",
+        "Implementing responsive design principles and ensuring cross-browser compatibility to enhance the user experience.",
+        "Participating actively in code reviews, offering constructive feedback, and contributing to the continuous improvement of development processes.",
+        "Utilizing Nestjs, Prisma, and RefineJS to optimize database interactions, enhance performance, and ensure scalability of web applications.",
+        "Integrating authentication and authorization mechanisms to enhance the security of the back-end systems.",
+        "Troubleshooting and debugging issues, ensuring timely resolution to maintain optimal application performance.",
+    ]
+}
+ 
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
